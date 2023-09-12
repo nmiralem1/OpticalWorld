6d19e9dcc4f73e44d75dda6fcac715bce1cd1c69
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-12 18:29:12 +0200

    Gitignore

--- src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Order;
+import ba.unsa.etf.rpr.domain.User;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public interface OrderDao extends Dao<Order> {
+    public int totalIncome() throws SQLException;
+    public List<Order> getAllForUser(User user) throws SQLException;
 }
