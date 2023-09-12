ed3a0399635a47cb29302df57171c3f4203a9516
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-12 18:27:59 +0200

    Gitignore

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.GlassesException;
+
+import java.sql.SQLException;
 
 public interface UserDao extends Dao<User> {
+    User findUsername(String usernameField) throws GlassesException;
+    public int totalUsers() throws SQLException;
 }
