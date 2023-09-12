7e965fc6c41497d4b7441f9df1e227cb47235af1
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-12 18:36:07 +0200

    Gitignore

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,129 +1,86 @@
 package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.GlassesException;
 
-import java.io.FileReader;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+import java.util.TreeMap;
 
-public class UserDaoSQLImpl implements UserDao {
-    private Connection connection;
+public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
+    private static UserDaoSQLImpl instance = null;
+    private UserDaoSQLImpl() {
+        super("users");
+    }
 
-    public UserDaoSQLImpl(){
-        try{
-            FileReader reader = new FileReader("src/main/resources/database.properties");
-            Properties p = new Properties();
-            p.load(reader);
-            String s1 = p.getProperty("url");
-            String s2 = p.getProperty("user");
-            String s3 = p.getProperty("password");
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(s1,s2,s3);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-}
+
+    public static UserDaoSQLImpl getInstance(){
+        if(instance==null)
+            instance = new UserDaoSQLImpl();
+        return instance;
+    }
+
+    public static void removeInstance(){
+        if(instance!=null)
+            instance=null;
+    }
 
     @Override
-    public User getById(int id) {
-        String query = "SELECT * FROM user WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setInt(1, id);
+    public User row2object(ResultSet rs) throws GlassesException{
+        try {
+            User person = new User();
+            person.setId(rs.getInt("id"));
+            person.setUsername(rs.getString("username"));
+            person.setFirstName(rs.getString("firstName"));
+            person.setLastName(rs.getString("lastName"));
+            person.setEmail(rs.getString("email"));
+            person.setRole(rs.getInt("isAdministrator"));
+            person.setPassword(rs.getString("password"));
+            return person;
+        } catch (Exception e) {
+            throw new GlassesException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
+    @Override
+    public Map<String, Object> object2row(User object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("username", object.getUsername());
+        item.put("firstName", object.getFirstName());
+        item.put("lastName", object.getLastName());
+        item.put("email", object.getEmail());
+        item.put("isAdministrator", object.getRole());
+        item.put("password", object.getPassword());
+        return item;
+    }
+
+    @Override
+    public User findUsername(String usernameField) throws GlassesException{
+        String find = "SELECT id from users where username='" + usernameField +"'";
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(find, Statement.RETURN_GENERATED_KEYS);
             ResultSet rs = stmt.executeQuery();
-            if (rs.next()){ // result set is iterator.
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setSurname(rs.getString("surname"));
-                user.setCreditCard(rs.getInt("size"));
-                user.setEmail(rs.getString("email"));
-                user.setContact(rs.getInt("contact"));
-                user.setAddress(rs.getString("address"));
-                rs.close();
-                return user;
-            }else{
-                return null; // if there is no elements in the result set return null
-            }
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
-    }
-
-    @Override
-    public User add(User item) {
-        String insert = "INSERT INTO user(name) VALUES(?)";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, item.getName());
-            stmt.executeUpdate();
-
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next(); // we know that there is one key
-            item.setId(rs.getInt(1)); //set id to return it back
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public User update(User item) {
-        String insert = "UPDATE user SET name = ? WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, item.getId());
-            stmt.setObject(2, item.getName());
-            stmt.setObject(3, item.getSurname());
-            stmt.setObject(4, item.getCreditCard());
-            stmt.setObject(5,item.getEmail());
-            stmt.setObject(6,item.getContact());
-            stmt.setObject(7,item.getAddress());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public void delete(int id) {
-        String insert = "DELETE FROM user WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, id);
-            stmt.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<User> getAll() {
-        String query = "SELECT * FROM user";
-        List<User> users = new ArrayList<User>();
-        try{
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){ // result set is iterator.
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setName(rs.getString("name"));
-                user.setSurname(rs.getString("surname"));
-                user.setCreditCard(rs.getInt("creditCard"));
-                user.setEmail(rs.getString("email"));
-                user.setContact(rs.getInt("contact"));
-                user.setAddress(rs.getString("address"));
-                users.add(user);
+            if(rs.next()) { // result set is iterator.
+                return getById(rs.getInt(1));
             }
             rs.close();
-        }catch (SQLException e){
-            e.printStackTrace(); // poor error handling
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return users;
+        return null;
+    }
+
+    public int totalUsers() throws SQLException{
+        int total = 0;
+        String query = "SELECT count(username) AS total_users FROM users";
+        try (PreparedStatement st = AbstractDao.getConnection().prepareStatement(query)) {
+            ResultSet result = st.executeQuery();
+            if (result.next()) total = result.getInt("total_users");
+        }
+        return total;
     }
 }
