ed9e8c37d18cae1ca9c356789e142dbbaca8c8cc
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-17 01:09:23 +0200

    Added java documentation

--- src/main/java/ba/unsa/etf/rpr/dao/GlassesDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GlassesDao.java
@@ -4,7 +4,11 @@ import ba.unsa.etf.rpr.domain.Glasses;
 
 import java.sql.SQLException;
 import java.util.List;
-
+/**
+ * Dao interface for Glasses domain bean
+ *
+ * @author nmiralem1
+ */
 public interface GlassesDao extends Dao<Glasses> {
 
     int totalGlasses() throws SQLException;
