c83db64b42faafd22f30e064000602ca7b0fcf4a
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-17 01:16:12 +0200

    Added java documentation

--- src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/OrderDao.java
@@ -4,7 +4,11 @@ import ba.unsa.etf.rpr.domain.User;
 
 import java.sql.SQLException;
 import java.util.List;
-
+/**
+ * Dao interface for Reservation domain bean
+ *
+ * @author nmiralem1
+ */
 public interface OrderDao extends Dao<Order> {
     int totalIncome() throws SQLException;
     List<Order> getAllForUser(User user) throws SQLException;
