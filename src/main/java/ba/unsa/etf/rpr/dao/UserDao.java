515d770dbf57beca60637ebe3828a60f179e5368
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-17 01:23:04 +0200

    Added java documentation

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -3,7 +3,11 @@ import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.GlassesException;
 
 import java.sql.SQLException;
-
+/**
+ * Dao interface for User domain bean
+ *
+ * @author nmiralem1
+ */
 public interface UserDao extends Dao<User> {
     User findUsername(String usernameField) throws GlassesException;
     int totalUsers() throws SQLException;
