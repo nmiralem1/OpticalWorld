4190a35914e3cac12733d7b6c3c84ee8577bd5a0
Nadina Miralem <115872381+nmiralem1@users.noreply.github.com>
2023-09-16 21:27:23 +0200

    added a help controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/HelpController.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+public class HelpController {
+
+    @FXML private Button aboutUsButton, signInButton, signUpButton, helpButton;
+    @FXML private ImageView goBack;
+    private final Utils utils = new Utils();
+
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
+        utils.changeWindow(signUpButton, "Sign Up", "/fxmlFiles/SignUp.fxml", new SignUpController());
+    }
+
+    public void aboutUsOnAction(ActionEvent actionEvent) throws IOException {
+        utils.changeWindow(aboutUsButton, "About Us", "/fxmlFiles/AboutUs.fxml", new AboutUsController());
+    }
+
+    @FXML
+    public void initialize() {
+        goBack.setOnMouseClicked(event -> utils.changeWindow(goBack, "Home Page", "/fxmlFiles/Home.fxml", new HomeController()));
+
+    }
+}
