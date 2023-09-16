360210ed42dc30f6f2d1f2165e83545edc86731d
Nadina Miralem <115872381+nmiralem1@users.noreply.github.com>
2023-09-16 22:24:40 +0200

    updated glasses dao

--- src/main/java/ba/unsa/etf/rpr/dao/GlassesDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GlassesDao.java
@@ -3,8 +3,11 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Glasses;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface GlassesDao extends Dao<Glasses> {
 
-    public int totalGlasses() throws SQLException;
+    int totalGlasses() throws SQLException;
+
+    List<Glasses> getAllByCategory(String category);
 }
