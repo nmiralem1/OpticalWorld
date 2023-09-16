fb4a2bbf3ad120c20da981a348bb4e89f574c42f
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-16 02:47:06 +0200

    added the admin panel

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.controllers;
+
+public class AdminPanelController {
+
+    private User user = new User();
+
+    public AdminPanelController(User user) {
+        this.user = user;
+    }
+
+    public AdminPanelController() {
+    }
+}
