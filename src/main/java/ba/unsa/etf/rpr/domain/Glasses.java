dc0d552e14a9a9faf959264824e71b5adad47192
nmiralem1 <nmiralem1@etf.unsa.ba>
2023-09-16 02:49:54 +0200

    changed the domain file

--- src/main/java/ba/unsa/etf/rpr/domain/Glasses.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Glasses.java
@@ -8,76 +8,58 @@ import java.util.Objects;
 
 public class Glasses implements Idable {
     private int id;
+    private String name, category, image;
+    private double price;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    private String name;
-    private String category;
-    private double price;
-
     public String getImage() {
         return image;
     }
-
     public void setImage(String image) {
         this.image = image;
     }
-
-    private String image;
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getCategory() {
         return category;
     }
-
     public void setCategory(String category) {
         this.category = category;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
-
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name "+ name +
-                ", category='" + category + '\'' +
-                ", price=" + price +
-
-                '}';
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Glasses product = (Glasses) o;
-        return id == product.id;
+        Glasses glasses = (Glasses) o;
+        return id == glasses.id && Double.compare(price, glasses.price) == 0 && Objects.equals(name, glasses.name) && Objects.equals(category, glasses.category) && Objects.equals(image, glasses.image);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(id, category, price);
+        return Objects.hash(id, name, category, image, price);
+    }
+    @Override
+    public String toString() {
+        return "Glasses{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
